723e1f4930427247c2ab8e925090ca6c176f3dd2
Murillo Marques <murillo.oliveira@foton.la>
2023-05-22 15:13:54 -0300

    Implementando DisplayName aos testes e classes de teste

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,15 +1,18 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testes no utilitário saudação")
 class SaudacaoUtilTest {
 
     private static final String SAUCADAO_INCORRETA = "Saudação incorreta!";
 
     @Test
+    @DisplayName("Deve saudar com bom dia")
     void deveSaudarBomDia() {
         int horaValida = 9;
         String saudacao = SaudacaoUtil.saudar(horaValida);
@@ -17,6 +20,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve saudar com bom dia às 5 horas")
     void deveSaudarBomDiaAPartir5h() {
         int horaValida = 5;
         String saudacao = SaudacaoUtil.saudar(horaValida);
@@ -24,6 +28,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve saudar com boa tarde")
     void deveSaudarBoaTarde() {
         int horaValida = 14;
         String saudacao = SaudacaoUtil.saudar(horaValida);
@@ -31,6 +36,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve saudar com boa noite")
     void deveSaudarBoaNoite() {
         int horaValida = 20;
         String saudacao = SaudacaoUtil.saudar(horaValida);
@@ -38,6 +44,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve saudar com boa noite às 4 horas")
     void deveSaudarBoaNoiteAs4h() {
         int horaValida = 4;
         String saudacao = SaudacaoUtil.saudar(horaValida);
@@ -45,6 +52,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve lançar exception caso hora inválida")
     void deveLancarException() {
         int horaInvalida = -10;
         // Executable do Jupiter
@@ -54,6 +62,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve ser um sucesso a requisição")
     void naoDeveLancarException() {
         int horaValida = 0;
         Executable chamadaValidaDeMetodo = () -> SaudacaoUtil.saudar(horaValida);
