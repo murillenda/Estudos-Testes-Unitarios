a05740a338c5e060484b78677303121268f336fa
Murillo Marques <murillo.oliveira@foton.la>
2023-05-22 16:22:24 -0300

    Usando BeforeEach e BeforeAll para cenarios com a preparacao de um cenario com muitas dependencias

---
+++ src/test/java/com/algaworks/junit/blog/negocio/CalculadoraGanhosTest.java
@@ -0,0 +1,52 @@
+package com.algaworks.junit.blog.negocio;
+
+import com.algaworks.junit.blog.modelo.Editor;
+import com.algaworks.junit.blog.modelo.Ganhos;
+import com.algaworks.junit.blog.modelo.Post;
+import com.algaworks.junit.blog.utilidade.ProcessadorTextoSimples;
+import org.junit.jupiter.api.*;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraGanhosTest {
+
+    static CalculadoraGanhos calculadora;
+    Editor autor;
+    Post post;
+
+    @BeforeAll
+    static void beforeAll() {
+        calculadora = new CalculadoraGanhos(new ProcessadorTextoSimples(), BigDecimal.TEN);
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        autor = new Editor(1L, "Murillo@gmail.com", "Murillo", new BigDecimal("5"),
+                true);
+
+        post = new Post(1L, "Ecossistema Java", "O ecossistema do java é muito maduro", autor,
+                "ecossistema-java-abc123", null, false, false);
+    }
+
+    @Test
+    void deveCalcularGanhos() {
+        Ganhos ganhos = calculadora.calcular(post);
+
+        assertEquals(new BigDecimal("45"), ganhos.getTotalGanho());
+        assertEquals(7, ganhos.getQuantidadePalavras());
+        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
+    }
+
+    @Test
+    void deveCalcularGanhosSemPremium() {
+        autor.setPremium(false);
+        Ganhos ganhos = calculadora.calcular(post);
+
+        assertEquals(new BigDecimal("35"), ganhos.getTotalGanho());
+        assertEquals(7, ganhos.getQuantidadePalavras());
+        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
+    }
+
+}
