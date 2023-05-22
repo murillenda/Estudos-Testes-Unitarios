d21f22e3ac6ba5f9233ea5d439a2c24fb3fccd03
Murillo Marques <murillo.oliveira@foton.la>
2023-05-22 14:53:05 -0300

    Modos de dar disable em um teste unitario, seja por variavel de ambiente ou nao

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,6 +1,9 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
@@ -9,7 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class SimuladorEsperaTest {
 
     @Test
+    // @Disabled("Não é mais aplicável")
+    // @EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
     void deveEsperarENaoDarTimeout() {
+        // Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")),
+                // () -> "Abortando teste: Não deve ser executado em PROD");
         // Método deveria executar em 1 segundo
         // Neste assertTimetout, ele só da erro quando o método termina de rodar
         assertTimeout(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
