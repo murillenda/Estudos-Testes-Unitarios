3c0a894e29d26ce892ea9db32a5803fbefdd204d
Murillo Marques <murillo.oliveira@foton.la>
2023-05-22 14:53:36 -0300

    Alguns testes unitarios e refactor de saudacaoUtil

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -6,10 +6,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SaudacaoUtilTest {
 
+    private static final String SAUCADAO_INCORRETA = "Saudação incorreta!";
+
     @Test
-    void saudar() {
-        String saudacao = SaudacaoUtil.saudar(9);
-        assertEquals("Bom dia", saudacao, "Saudação incorreta!");
+    void deveSaudarBomDia() {
+        int horaValida = 9;
+        String saudacao = SaudacaoUtil.saudar(horaValida);
+        assertEquals("Bom dia", saudacao, SAUCADAO_INCORRETA);
+    }
+
+    @Test
+    void deveSaudarBomDiaAPartir5h() {
+        int horaValida = 5;
+        String saudacao = SaudacaoUtil.saudar(horaValida);
+        assertEquals("Bom dia", saudacao, SAUCADAO_INCORRETA);
+    }
+
+    @Test
+    void deveSaudarBoaTarde() {
+        int horaValida = 14;
+        String saudacao = SaudacaoUtil.saudar(horaValida);
+        assertEquals("Boa tarde", saudacao, SAUCADAO_INCORRETA);
+    }
+
+    @Test
+    void deveSaudarBoaNoite() {
+        int horaValida = 20;
+        String saudacao = SaudacaoUtil.saudar(horaValida);
+        assertEquals("Boa noite", saudacao, SAUCADAO_INCORRETA);
+    }
+
+    @Test
+    void deveSaudarBoaNoiteAs4h() {
+        int horaValida = 4;
+        String saudacao = SaudacaoUtil.saudar(horaValida);
+        assertEquals("Boa noite", saudacao, SAUCADAO_INCORRETA);
     }
 
     @Test
