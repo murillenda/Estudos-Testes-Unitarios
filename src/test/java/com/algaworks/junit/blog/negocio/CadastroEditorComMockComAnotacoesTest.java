7c4d3d7749cd92bf128fe4e75f4114d1a11d4dcf
Murillo Marques <giikthelol@gmail.com>
2023-05-24 21:19:55 -0300

    Alterando estado de parametro passados nos mocks, parametros dinamicos, verificando chamada de metodos com verify e forcando uma exception com mock

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockComAnotacoesTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockComAnotacoesTest.java
@@ -2,10 +2,7 @@ package com.algaworks.junit.blog.negocio;
 
 import com.algaworks.junit.blog.armazenamento.ArmazenamentoEditor;
 import com.algaworks.junit.blog.modelo.Editor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class) // Extensão passada paras anotações do @Mock serem injetados
@@ -38,14 +36,44 @@ class CadastroEditorComMockComAnotacoesTest {
     void init() {
         editor = new Editor(null, "Murillo", "murillo@email.com", BigDecimal.TEN, true);
 
-        Mockito.when(armazenamentoEditorMock.salvar(editor))
-                .thenReturn(new Editor(1L, "Murillo", "murillo@email.com", BigDecimal.TEN, true));
+        // Mockito.any é muito útil em diversos casos principalmente quando temos muitos parâmetros
+        // nos métodos que estamos realizando o mock
+        Mockito.when(armazenamentoEditorMock.salvar(Mockito.any(Editor.class)))
+                // Alterando um parâmetro passado para ficar mais dinâmico
+                // Esse método thenAnswer pega a própria invocação do método
+                .thenAnswer(invocationOnMock -> {
+                    Editor editorPassado = invocationOnMock.getArgument(0, Editor.class);
+                    editorPassado.setId(1L);
+                    return editorPassado;
+                });
     }
 
     @Test
     void Dado_um_editor_valido_Quando_criar_Entao_deve_retornar_um_id_de_cadastro() {
         Editor editorSalvo = cadastroEditor.criar(editor);
         assertEquals(1L, editorSalvo.getId());
+    }
+
+    @Test
+    void Dado_um_editor_valido_Quando_criar_Entao_deve_chamar_metodo_salvar_do_armazenamento() {
+        // Verificar se o método realmente está sendo chamado e passando o parâmetro de editor
+        cadastroEditor.criar(editor);
+        // verify(mock, numero de vezes chamado)
+        Mockito.verify(armazenamentoEditorMock, Mockito.times(1))
+                .salvar(Mockito.eq(editor)); // Também pode usar Mockito.any(Editor.class), usando assim só pra ter certeza que é o parâmetro que estamos passando
+    }
+
+    // Verificar se realmente não envia email em caso de exception
+    @Test
+    void Dado_um_editor_Quando_criar_e_lancar_exception_ao_salvar_Entao_nao_deve_enviar_email() {
+        // Forçando a retornar RunTimeException
+        Mockito.when(armazenamentoEditorMock.salvar(editor)).thenThrow(new RuntimeException());
+        assertAll("Não deve enviar e-mail quando lançar Exception do armazenamento",
+                // Verificando se lançou RunTimeException
+                () -> Assertions.assertThrows(RuntimeException.class, () -> cadastroEditor.criar(editor)),
+                // Verificando se não passou no enviarEmail
+                () -> Mockito.verify(gerenciadorEnvioEmailMock, Mockito.never()).enviarEmail(Mockito.any()));
+        // Sem o assertAll ele lança exceção e não chega no verify
     }
 
 }
